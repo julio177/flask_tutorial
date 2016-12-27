8af51c70f485517e53d5f7a38576b06d0a539cd4
julio177 <julioc.aguilarv@gmail.com>
2016-12-27 16:19:06 -0600

    updates requirements

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask',
+        'flask-sqlalchemy',
+        'flask-migrate',
+        'pygresql',
+        'flask-security'
     ],
 )
