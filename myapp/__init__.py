70c68f376d17763281d54b9ba0dc899fabdf690a
julio177 <julioc.aguilarv@gmail.com>
2016-12-28 11:50:43 -0600

    implementa flask security

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security
 from .blue import blue
 from .views.red import red
 
@@ -9,3 +11,7 @@ app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
 from .views import hello
+from myapp.models import db, user_datastore
+
+db.init_app(app)
+security = Security(app, user_datastore)
--- myapp/views/hello.py
+++ myapp/views/hello.py
@@ -1,5 +1,9 @@
 from myapp import app
+from flask_security import login_required
+from myapp.models import db, user_datastore
+
 
 @app.route('/')
+@login_required
 def index():
     return 'Hello World!'
