61f866f27f5c5e310ad4a69a298a21501fb4c788
julio177 <julioc.aguilarv@gmail.com>
2016-12-28 12:29:03 -0600

    cambia formato de import

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_security import SQLAlchemyUserDatastore, \
-    UserMixin, RoleMixin
+from flask_security import SQLAlchemyUserDatastore, UserMixin, RoleMixin
 
 db = SQLAlchemy()
 
